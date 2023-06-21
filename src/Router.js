da2414aaf8b0811f146ecfc33cfa117e9665d1d5
steven mccombe <smccombe93@gmail.com>
2023-06-21 09:27:51 -0400

    updated routing

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import WelderProfilePage from './pages/WelderProfilePage';
 import EmployerProfilePage from './pages/EmployerProfilePage';
@@ -9,13 +9,13 @@ import ChatPage from './pages/ChatPage';
 function Router() {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/welder/:id" component={WelderProfilePage} />
-                <Route path="/employer/:id" component={EmployerProfilePage} />
-                <Route path="/job/:id" component={JobPage} />
-                <Route path="/chat" component={ChatPage} />
-            </Switch>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/welder/:id" element={<WelderProfilePage />} />
+                <Route path="/employer/:id" element={<EmployerProfilePage />} />
+                <Route path="/job/:id" element={<JobPage />} />
+                <Route path="/chat" element={<ChatPage />} />
+            </Routes>
         </BrowserRouter>
     );
 }
